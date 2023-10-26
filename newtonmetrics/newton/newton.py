ccc81c3ee1fa8f435fd96c22783084ccd9420c9c
Joker84a <lucapodo97@gmail.com>
2023-10-26 22:11:54 +0200

    d

--- newtonmetrics/newton/newton.py
+++ newtonmetrics/newton/newton.py
@@ -36,7 +36,8 @@ class Newton(object):
     def compute_score(self, df_path, vegazero, groundtruth):
         
         df = pd.read_csv(df_path, index_col=0)
-        df = self.sanitize_column_names(df)
+   
+        # df = self.sanitize_column_names(df)
 
         isCompling = 0
         l_hard = 0.9
@@ -65,67 +66,89 @@ class Newton(object):
             sim = self.vz.vega_zero_groundtruth_similarity_score(vegazero, groundtruth)
             res['sim'] = sim
 
-            try:
-                schema: dict = schema_from_dataframe(df) #Generating the data schema to extract the field types automatically
-                spec = dict_to_facts(schema | vegalite_gen_) #converte in fact e concatena data e vis
-                isVisCorrect = self.draco.check_spec(spec)
-                res['isVisCorrect'] = isVisCorrect
-                violations = len(self.draco.get_violations(spec))
-                res['violations']=violations
+            isMarkCorrect = vegazero_spec_['mark'] == vegazero_ground_spec_['mark']
+            res['isMarkCorrect'] = isMarkCorrect
+            isXCorrect = vegazero_spec_['encoding']['x'] == vegazero_ground_spec_['encoding']['x']
+            res['isXCorrect'] = isXCorrect
+            isYCorrect = vegazero_spec_['encoding']['y']['y'] == vegazero_ground_spec_['encoding']['y']['y']
+            res['isYCorrect'] = isYCorrect
 
-                isMarkCorrect = vegazero_spec_['mark'] == vegazero_ground_spec_['mark']
-                res['isMarkCorrect'] = isMarkCorrect
-                isXCorrect = vegazero_spec_['encoding']['x'] == vegazero_ground_spec_['encoding']['x']
-                res['isXCorrect'] = isXCorrect
-                isYCorrect = vegazero_spec_['encoding']['y']['y'] == vegazero_ground_spec_['encoding']['y']['y']
-                res['isYCorrect'] = isYCorrect
+            # try:
+            schema: dict = schema_from_dataframe(df) #Generating the data schema to extract the field types automatically
+            spec = dict_to_facts(schema | vegalite_gen_) #converte in fact e concatena data e vis
+            isVisCorrect = self.draco.check_spec(spec)
+            res['isVisCorrect'] = isVisCorrect
+            violations = len(self.draco.get_violations(spec))
+            res['violations']=violations
 
-                score = -l_hard*(1-isVisCorrect)+ l_sim * sim - l_soft * violations + l_acc * (isMarkCorrect + isXCorrect + isYCorrect)
-                if (score>=0):
-                    res['score'] =  self.NormalizeData(score)
-                else:
-                    res['score'] = 0
+            score = -l_hard*(1-isVisCorrect)+ l_sim * sim - l_soft * violations + l_acc * (isMarkCorrect + isXCorrect + isYCorrect)
 
-            except:
-                res['isVisCorrect'] = 0
+            if (score>=0):
+                res['score'] =  self.NormalizeData(score)
+            else:
+                res['score'] = 0
+
         except Exception as e:
+            print(e)
             res['isCompiled'] = 0
-
-            
-        # sim = vz.vega_zero_groundtruth_similarity_score(vegazero, groundtruth)
-        
-        # schema: dict = schema_from_dataframe(df) #Generating the data schema to extract the field types automatically
-        # spec = dict_to_facts(schema | vegalite_gen_) #converte in fact e concatena data e vis
-        # isVisCorrect = self.draco.check_spec(self.spec)
-        # violations = len(self.draco.get_violations(self.spec))
-
-        # isMarkCorrect = vegazero_spec_['mark'] == vegazero_ground_spec_['mark']
-        # isXCorrect = vegazero_spec_['encoding']['x'] == vegazero_ground_spec_['encoding']['x']
-        # isYCorrect = vegazero_spec_['encoding']['y']['y'] == vegazero_ground_spec_['encoding']['y']['y']
-
-        # score = -l_hard*(1-isVisCorrect)+ l_sim * sim - l_soft * violations + l_acc * (isMarkCorrect + isXCorrect + isYCorrect)
-
-        # res = {
-        #     "isCompiled": 1, 
-        #     "isVisCorrect": isVisCorrect,
-        #     "sim": sim,
-        #     "violations": violations,
-        #     "isMarkCorrect": isMarkCorrect,
-        #     "isXCorrect": isXCorrect,
-        #     "isYCorrect":isYCorrect
-        # }
+       
         return res 
-        # if (score>=0):
-        #         return [self.NormalizeData(score), res]
-        # else:
-        #     return [0, res]
-
+        
     def test(self, path):
         vegazero = "mark area data payments encoding x payment_type_code y aggregate average amount_paid group x"
         ground = "mark area data payments encoding x payment_type_code y aggregate average amount_paid group x"
 
 
-        print(self.compute_score(path, vegazero, ground)[0])
-        print(self.compute_score(path, vegazero, ground)[1])
+        # print(self.compute_score(path, vegazero, ground)[0])
+        # print(self.compute_score(path, vegazero, ground)[1])
+
+    def test_draco(self):
+        import pandas as pd
+        import os
+        eval_df = pd.read_csv("/Users/luca/Documents/Dottorato/LLM4VIS/Newton/data/raw/Newton/evaluation.csv")
+        newton_df = pd.read_csv("/Users/luca/Documents/Dottorato/LLM4VIS/Newton/data/final/NewtonLLM dataset/newtow_test.csv")
+
+        # for i in range(100, len(eval_df)):
+        #     print(i)
+        i=100
+        try:
+            ground = eval_df['groundtruth'][i]
+            prediction = eval_df['prediction'][i]
+
+            id = newton_df[newton_df['output'] == ground]['tvBench_id'].values[0]
+            path = os.path.join('/Users/luca/Documents/Dottorato/LLM4VIS/Newton/data/final/NewtonLLM dataset/datasets', id +'.csv')
+            df = pd.read_csv(path)
+
+            vegalite_gen_, _ = self.vz.to_VegaLite(prediction)
+
+            schema: dict = schema_from_dataframe(df) #Generating the data schema to extract the field types automatically
+            spec = dict_to_facts(schema | vegalite_gen_) #converte in fact e concatena data e vis
+          
+            if self.draco.check_spec(spec):
+                print('ciao')
+            else: 
+                print('errore')
+            # if self.draco.check_spec(spec):
+            #     print('cioa')
+            #     yield spec
+            # else: 
+            #     print(id)
+            
+        except Exception as e:
+            print(e)
+            pass
+            # break
+
+
+
+
+
+
+n = Newton()
+
+vegazero =  "mark area data apartment_bookings encoding x booking_start_date y aggregate count booking_start_date transform group x sort y asc"
+ground = "mark area data apartment_bookings encoding x booking_start_date y aggregate count booking_start_date transform group x sort y asc"
+
+n.test_draco()
     
     
